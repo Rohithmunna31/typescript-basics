fedeb0098b2511463da4fde81a36c1e35b142b1f
Rohithmunna31 <rohithkasna31@gmail.com>
2024-03-24 07:05:29 +0530

    Moved all js files to /dist and all ts files to /src to maintain project structure

--- dist/routes/todos.js
+++ dist/routes/todos.js
@@ -1,38 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var todos = [];
-var router = (0, express_1.Router)();
-router.get("/", function (req, res, next) {
+const express_1 = require("express");
+let todos = [];
+const router = (0, express_1.Router)();
+router.get("/", (req, res, next) => {
     res.status(200).json({ message: "success", todos: todos });
 });
-router.post("/todo", function (req, res, next) {
-    var newTodo = {
+router.post("/todo", (req, res, next) => {
+    const newTodo = {
         id: new Date().toISOString(),
         name: req.body.text,
     };
     todos.push(newTodo);
     res.status(200).json({ success: true, todos: newTodo });
 });
-router.put("/todo/:todoId", function (req, res, next) {
-    var id = req.params.todoId;
-    var todoIndex = todos.findIndex(function (todoitem) { return todoitem.id === id; });
+router.put("/todo/:todoId", (req, res, next) => {
+    const id = req.params.todoId;
+    const todoIndex = todos.findIndex((todoitem) => todoitem.id === id);
     if (todoIndex >= 0) {
         todos[todoIndex] = { id: todos[todoIndex].id, name: req.body.text };
         return res.status(200).json({ message: "updated todo", todos: todos });
     }
     res.status(400).json({ message: "cannot find todo for this id" });
 });
-router.delete("/todo/:todoId", function (req, res, next) {
-    if (parseInt(req.params.todoId) >= 0) {
-        todos = todos.filter(function (todoItem) { return todoItem.id !== req.params.todoId; });
+router.delete("/todo/:todoId", (req, res, next) => {
+    const todoIndex = todos.findIndex((todoitem) => todoitem.id === req.params.todoId);
+    if (todoIndex) {
+        todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
         return res
             .status(201)
             .json({ success: true, message: "Todo deleted successfully" });
     }
-    res
-        .status(400)
-        .json({
+    res.status(404).json({
         success: false,
         message: "The todo u want to delete does not exist",
     });
